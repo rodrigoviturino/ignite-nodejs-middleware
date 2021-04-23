8378d9deccfde158fe72b539687a97ccd3c3d61b
Rodrigo Viturino <rodrigo.souza@spread.com.br>
2021-04-23 14:00:14 -0300

    Criando middleware responsavel por buscar usuario por ID na rota

--- src/index.js
+++ src/index.js
@@ -72,7 +72,18 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if(!user){
+    return response.status(400).json({
+      error: "User not found!"
+    });
+  }
+  request.user = user;
   
+  return next();
 }
 
 app.post('/users', (request, response) => {
